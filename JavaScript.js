aa348028b856bd58e233c8d92e15fb7e8e6a7273
raguramkc-26 <raguramkc038@gmail.com>
2026-01-21 10:10:40 +0530

    Initial commit

---
+++ JavaScript.js
@@ -0,0 +1,160 @@
+//let a=8;  //assign 
+//a=7; //Reassign
+//console.log(a)
+
+//const a=9;
+//console.log(a)
+
+//var a=10;
+//var a="ten";
+//console.log(a)
+
+//const a = 10 + 10;
+//console.log(a)
+
+//DataType
+//const age = 25;
+//const isAdult = true;
+//const word = `apple`;
+//console.log(typeof age);
+//console.log(typeof isAdult);
+//console.log(typeof word);
+
+//Array
+//let hobbies = ["Painting" , "Planting" , "Washing"];
+//console.log(hobbies);
+//console.log(typeof hobbies);
+
+//js Object or Json
+//let person = {
+  //  name: "KC RAGURAM",
+  //  age:25,
+  //  hobbies:["Painting" , "Planting" , "Washing"]
+//};
+//console.log(person);
+//console.log(typeof person) 
+
+//let age = 25;
+//age++;
+//console.log(age);
+
+//console.log(5 == '5');
+
+//Logical Operator concept is used to check magalir urimai thoogai scheme
+//const age = 15;      //not a user friendly
+//const hasAadhar = false;
+//const hasIncomeCertificate = true;
+//console.log(age >=21 && hasAadhar == true || hasIncomeCertificate == true);
+
+//Logical Operator using conditional statement
+//const age = 20;
+//const hasAadhar = true;
+//const hasIncomeCertificate = true;
+//if(age >=21 && (hasAadhar == true || hasIncomeCertificate == true)) {
+  //  console.log("Eligible for Scheme")
+//}else {
+  //  console.log("Not Eligible for Scheme")
+//}
+
+//if statement
+//let number=-5;
+//if (number < 0) {
+  //  number = number *  -1;
+//}
+//console.log(number)
+
+//if else statement
+//let number = 5;
+//if (number > 0) {
+  //  console.log(number, 'is a positive number' )
+//}
+//else{
+  //  console.log(number, 'is a negative number')
+//}
+
+//nested if else statement
+//let number = 6;
+//if (number > 0) {
+ //number > 0
+//console.log(number, 'is a positive number');
+//} else {
+ //number > 0 
+ //either number < 0 or number == 0
+    // the number can either be a 0 or a negative number
+     // decision statement??
+  //   if (number == 0) {
+  //       console.log(number, 'is a zero');
+  //   } else {
+    //     console.log(number, 'is a negative number');
+    // }
+ //}
+
+ //multiple if else if else condition
+ //let number = 0 
+ //if (number > 0){ 
+//console.log(number, "is a Positive Number");
+// } else if(number < 0) {
+//console.log(number, "is a Negative Number");
+// } else {
+//  console.log(number, "is a Zero");
+// } 
+
+//Switch Case condition
+//let number = 2;
+//switch(number) {
+//  case 1:
+//    console.log("one");
+//    break;
+//  case 2:
+//    console.log("two");
+//    break;
+//  case 3:
+//    console.log("three");
+//    break;
+//  case 4:
+//    console.log("four");
+//    break;
+//  default:
+//    console.log("only 1-4 numbers are allowed");
+//    break;
+//}
+
+//Ternary operator
+// let number = 6;
+// // ternary operator
+// // ?:
+// number > 0 ? console.log(number, 'is a positive number') : number < 0 ? console.log(number, 'is a negative number') : console.log(number, 'is a zero');
+
+//Looping Statement
+//if condition is true -> loops runs
+//if condition is false -> loops stops
+
+//While loop
+//let count = 0;
+//while(count < 3) {     //using less than Relational operator 
+  //console.log('Hello');
+  //count ++;
+//}
+
+//let count = 1;
+//while(count !=4) { //Using Not Operator
+  //console.log("Hello");
+  //count ++;
+//}
+
+//for loop -> elegant syntax
+//for(let count = 0; count < 3; count ++) {
+//console.log("Hello");
+//}
+
+//do...while loop
+//let count = 0;
+//do {
+ // console.log("Hello");
+ // count ++;
+//} while (count <3);
+
+
+
+
+
